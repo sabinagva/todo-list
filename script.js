b2f078be39f0d6feaa7e582a901cdfadd83ccd45
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-05-05 14:32:20 -0500

    done

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ function onReady() {
     //register event handler on form's submit button
     //when that button is clicked we will run handleSubmit function
     $('#submit-btn').on('click',handleSubmit)
+    $('#to-do-list').on('click', '.delete-button', handleDelete)
 }
-
+let todoCount = 0;
 function handleSubmit(event) {
     console.log('submit click');
     //prevent default form button behavior 
     //which is undesirable page refresh(makes our console.log disappear)
     event.preventDefault()
+    todoCount++;
+    $('#to-do-count').text(todoCount)
 
     //grab values within inputs
     //.val() is getter of value
@@ -18,11 +21,19 @@ function handleSubmit(event) {
     let toDoInput = $('#to-do-input').val();
     let authorInput = $('#author-input').val();
 
-    console.log('to do:', toDoInput, 'author', authorInput);
+    console.log('to do:', toDoInput, 'author:', authorInput);
 
     //append values to the DOM
     //`${}`- helps us use js codes in html
-    $('#to-do-list').append(`<li>${authorInput} needs to do ${toDoInput}</li>`)
+    //text changes by overriding the value but append adds them together
+    $('#to-do-list').append(`<li>${authorInput} needs to do ${toDoInput} <button class="delete-button">Delete</button></li>`)
     $('#to-do-input').val('');
     $('#author-input').val('');
+      
+}
+
+function handleDelete() {
+    //delete my list
+    console.log('delete')
+    $(this).parent().remove();
 }
